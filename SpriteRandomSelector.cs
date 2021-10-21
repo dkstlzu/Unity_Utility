e6bdb1d6a1cce41e7a78819f687b850c9def93ca
Shin SeockJu <margron@naver.com>
2021-10-21 19:46:48 +0900

    SpriteRandomSelector added

---
+++ SpriteRandomSelector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utility
+{
+    public class SpriteRandomSelector : MonoBehaviour
+    {
+        public List<Sprite> SpriteList;
+        public SpriteRenderer SpriteRenderer;
+
+        void OnEnable()
+        {
+            SpriteRenderer.sprite = SpriteList[Random.Range(0, SpriteList.Count)];
+        }
+    }
+}
