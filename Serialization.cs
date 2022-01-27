cf83cfbafe9a60a5007b58c324123f1f661857b4
Shin SeockJu <margron@naver.com>
2022-01-27 16:43:52 +0900

    Serialization Utilities added

---
+++ Serialization.cs
@@ -0,0 +1,15 @@
+using System;
+
+using UnityEngine;
+using UnityEditor;
+
+namespace Utility
+{
+    public class Serialization
+    {
+        [MenuItem("CustomMenu/Print SerialzedProperties")]
+        static void PrintProperies()
+        {
+        }
+    }
+}
