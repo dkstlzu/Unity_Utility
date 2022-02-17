6c81b9f9b17807af1ec13697c0a3655598b74288
Shin SeockJu <margron@naver.com>
2022-02-17 15:17:04 +0900

    Now relative to Bezier Curve features all dealwith this class

--- BezierCurve.cs
+++ BezierCurve.cs
@@ -27,10 +27,94 @@ namespace Utility
         }
 
         public BGCurve Curve;
+        public BGCcCursor Cursor
+        {
+            get 
+            {
+                BGCcCursor cursor = null;
+                if (!gameObject.TryGetComponent<BGCcCursor>(out cursor))
+                {
+                    Debug.LogWarning($"{gameObject.name} do not have BGCcCursor");
+                }
+                return cursor;
+            }
+        }
 
         public BezierCurve(BGCurve curve)
         {
             Curve = curve;
+        }
+
+        public BGCcMath AddMath()
+        {
+            return AddBGCc<BGCcMath>() as BGCcMath;
+        }
+
+        public BGCcCursor AddCursor()
+        {
+            return AddBGCc<BGCcCursor>() as BGCcCursor;
+        }
+
+        public BGCcSplitterPolyline AddSplitPolyline()
+        {
+            return AddBGCc<BGCcSplitterPolyline>() as BGCcSplitterPolyline;
+        }
+
+        public BGCcSweep2D AddSweep2D()
+        {
+            return AddBGCc<BGCcSweep2D>() as BGCcSweep2D;
+        }
+
+        public BGCcTriangulate2D AddTriangulate2D()
+        {
+            return AddBGCc<BGCcTriangulate2D>() as BGCcTriangulate2D;
+        }
+
+        public BGCcTrs AddTRS(Transform transform)
+        {
+            var c = AddBGCc<BGCcTrs>() as BGCcTrs;
+            c.ObjectToManipulate = transform;
+            return c;
+        }
+
+        public BGCcCursorChangeLinear AddCursorChangeLinear()
+        {
+            return AddBGCc<BGCcCursorChangeLinear>() as BGCcCursorChangeLinear;
+        }
+
+        public BGCcCursorObjectRotate AddCursorRotate(Transform transform = null)
+        {
+            var c = AddBGCc<BGCcCursorObjectRotate>() as BGCcCursorObjectRotate;
+            c.ObjectToManipulate = transform;
+            return c;
+        }
+
+        public BGCcCursorObjectScale AddCursorScale(Transform transform)
+        {
+            var c = AddBGCc<BGCcCursorObjectScale>() as BGCcCursorObjectScale;
+            c.ObjectToManipulate = transform;
+            return c;
+        }
+
+        public BGCcCursorObjectTranslate AddCursorTranslate(Transform transform)
+        {
+            var c = AddBGCc<BGCcCursorObjectTranslate>() as BGCcCursorObjectTranslate;
+            c.ObjectToManipulate = transform;
+            return c;
+        }
+
+        private BGCc AddBGCc<T>() where T : BGCc
+        {
+            T Component = null;
+            if (gameObject.TryGetComponent<T>(out Component))
+            {
+                Debug.LogWarning($"{gameObject.name} already has {Component.GetType()}");
+            } else
+            {
+                Component = gameObject.AddComponent<T>();
+            }
+
+            return Component;
         }
 
         public static BezierCurve ParabolaFromTo(Transform fromTransform, bool useFromTransform, Transform toTransform, bool useToTransform)
@@ -70,9 +154,9 @@ namespace Utility
 
             Vector3 middlePoint = (toTransform.position + fromTransform.position) / 2;
             bezierCurve.Curve[1].ControlFirstLocal = -factor * Vector3.Cross(dotPosition - middlePoint, Vector3.forward)/4;
-            var BGCcMath = newCurveObject.AddComponent<BGCcMath>();
-            var BGCcCursor = newCurveObject.AddComponent<BGCcCursor>();
-            var BGCcCursorObjectTranslate = newCurveObject.AddComponent<BGCcCursorObjectTranslate>();
+            // var BGCcMath = newCurveObject.AddComponent<BGCcMath>();
+            // var BGCcCursor = newCurveObject.AddComponent<BGCcCursor>();
+            // var BGCcCursorObjectTranslate = newCurveObject.AddComponent<BGCcCursorObjectTranslate>();
 
             Curves.Add(bezierCurve);
             return bezierCurve;
