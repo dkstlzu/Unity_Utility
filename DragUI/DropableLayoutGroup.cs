e1428e934713b9643317b12231b6b1c907eb85c8
Shin SeockJu <margron@naver.com>
2022-06-30 17:56:15 +0900

    DropableLayoutGroup Events now use ?.invoke instead ()

--- DragUI/DropableLayoutGroup.cs
+++ DragUI/DropableLayoutGroup.cs
@@ -31,15 +31,13 @@ namespace Utility.UI
                 // BeforeDropCallBack(DragAndDropableUI.DragingUI, eventData);
                 DragAndDropableUI.DragingUI.transform.SetParent(LayoutGroup.transform);
                 DragAndDropableUI.DragingUI.SuccessfullyDroped = true;
-                OnDropCallBack(DragAndDropableUI.DragingUI, eventData);
-                OnLateDropCallBack(DragAndDropableUI.DragingUI, eventData);
-                OnLateBoolDropCallBack(DragAndDropableUI.DragingUI, eventData);
+                OnDropCallBack?.Invoke(DragAndDropableUI.DragingUI, eventData);
+                OnLateDropCallBack?.Invoke(DragAndDropableUI.DragingUI, eventData);
             }
         }
 
         // public Action<DragAndDropableUI, PointerEventData> BeforeDropCallBack;
         public Action<DragAndDropableUI, PointerEventData> OnDropCallBack;
         public Action<DragAndDropableUI, PointerEventData> OnLateDropCallBack;
-        public Func<DragAndDropableUI, PointerEventData, bool> OnLateBoolDropCallBack;
     }
 }
