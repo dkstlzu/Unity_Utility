6b6eebfe9d141a26ddea4bc59d4423e95f717160
SeockJu Shin <dkstlzu@gmail.com>
2022-07-07 03:15:34 +0900

    Singleton GerOrCreateInstance added

--- Singleton.cs
+++ Singleton.cs
@@ -18,8 +18,29 @@ namespace Utility
                         _instance = FindObjectOfType(typeof(T)) as T;
                     } catch (NullReferenceException e)
                     {
-                        
                         Debug.LogError(e.ToString() + "\nThere's no active " + typeof(T) + " in this scene");
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public static T GetOrCreateInstance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    try
+                    {
+                        _instance = FindObjectOfType(typeof(T)) as T;
+                        if (_instance == null) throw new NullReferenceException();
+                    } catch (NullReferenceException e)
+                    {
+                        Debug.LogWarning("There's no active " + typeof(T) + " in this scene\nBut made default one");
+                        GameObject go = new GameObject(typeof(T).ToString() + "Singleton");
+                        _instance = go.AddComponent<T>();
                     }
                 }
 
