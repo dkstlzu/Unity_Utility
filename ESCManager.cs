0db65255f874fa73ab1a6d379fda88b69d77ac72
SeockJu Shin <dkstlzu@gmail.com>
2022-07-15 14:13:45 +0900

    ESCManager trace ESC input by itself

--- ESCManager.cs
+++ ESCManager.cs
@@ -1,11 +1,52 @@
 using System;
 
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+using Context = UnityEngine.InputSystem.InputAction.CallbackContext;
+
 namespace Utility
 {
     public class ESCManager : Singleton<ESCManager>
     {
+
+#if ENABLE_INPUT_SYSTEM
+        public InputAction ESCInputAction = new InputAction("ESCInputAction", InputActionType.Button, "<Keyboard>/escape");
+
+        void Awake()
+        {
+            ESCInputAction.performed += ESC;
+        }
+
+        void ESC(Context context)
+        {
+            ESC();
+        }
+
+        void OnEnable()
+        {
+            ESCInputAction.Enable();
+        }
+
+        void Disable()
+        {
+            ESCInputAction.Disable();
+        }
+#elif ENABLE_LEGACY_INPUT_MANAGER
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                ESC();
+            }
+        }
+#endif
+
         class ActionPriorityQueue : PriorityQueue<Action> {}
         private ActionPriorityQueue PQ = new ActionPriorityQueue();
+
+
         public void ESC()
         {
             PriorityQueue<Action>.Item item = PQ.Pop();
