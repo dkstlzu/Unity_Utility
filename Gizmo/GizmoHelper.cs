f611a2a783749961eb3cd7d991a07000a7be8999
SeockJu Shin <dkstlzu@gmail.com>
2022-07-24 16:16:13 +0900

    Gizmo editor script preprocessed by #if Unity_Editor

--- Gizmo/GizmoHelper.cs
+++ Gizmo/GizmoHelper.cs
@@ -1,29 +1,32 @@
+#if UNITY_EDITOR
 using UnityEngine;
+using UnityEditor;
 
 namespace Utility
 {
     public class GizmoHelper
     {
         public static void DrawString(string text, Vector3 worldPos, Color? colour = null) {
-             UnityEditor.Handles.BeginGUI();
- 
-             var restoreColor = GUI.color;
- 
-             if (colour.HasValue) GUI.color = colour.Value;
-             var view = UnityEditor.SceneView.currentDrawingSceneView;
-             Vector3 screenPos = view.camera.WorldToScreenPoint(worldPos);
- 
-             if (screenPos.y < 0 || screenPos.y > Screen.height || screenPos.x < 0 || screenPos.x > Screen.width || screenPos.z < 0)
-             {
-                 GUI.color = restoreColor;
-                 UnityEditor.Handles.EndGUI();
-                 return;
-             }
- 
-             Vector2 size = GUI.skin.label.CalcSize(new GUIContent(text));
-             GUI.Label(new Rect(screenPos.x - (size.x / 2), -screenPos.y + view.position.height + 4, size.x, size.y), text);
-             GUI.color = restoreColor;
-             UnityEditor.Handles.EndGUI();
+            Handles.BeginGUI();
+
+            var restoreColor = GUI.color;
+
+            if (colour.HasValue) GUI.color = colour.Value;
+            var view = SceneView.currentDrawingSceneView;
+            Vector3 screenPos = view.camera.WorldToScreenPoint(worldPos);
+
+            if (screenPos.y < 0 || screenPos.y > Screen.height || screenPos.x < 0 || screenPos.x > Screen.width || screenPos.z < 0)
+            {
+                GUI.color = restoreColor;
+                Handles.EndGUI();
+                return;
+            }
+
+            Vector2 size = GUI.skin.label.CalcSize(new GUIContent(text));
+            GUI.Label(new Rect(screenPos.x - (size.x / 2), -screenPos.y + view.position.height + 4, size.x, size.y), text);
+            GUI.color = restoreColor;
+            Handles.EndGUI();
          }
     }
 }
+#endif
