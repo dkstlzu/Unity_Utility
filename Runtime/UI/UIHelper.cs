0b3f8e79679cc9ab94cf4a3241ad62f9138da184
Shin SeockJu <margron@naver.com>
2022-08-09 04:10:50 +0900

    UIHelper ScaleOpen and ScaleClose now also use AnimationCurve
    AnimationCurveHelperClass added

--- Runtime/UI/UIHelper.cs
+++ Runtime/UI/UIHelper.cs
@@ -10,36 +10,80 @@ namespace dkstlzu.Utility
     {
         public static TaskManagerTask ScaleOpen(RectTransform rectTransform, float time)
         {
-            return new TaskManagerTask(ScaleCoroutine(rectTransform, time), true);
+            return new TaskManagerTask(ScaleTimeOpenCoroutine(rectTransform, time), true);
+        }
+
+        public static TaskManagerTask ScaleOpen(RectTransform rectTransform, AnimationCurve curve, float timeMultiplier = 1)
+        {
+            return new TaskManagerTask(ScaleCurveOpenCoroutine(rectTransform, curve, timeMultiplier), true);
         }
 
         public static TaskManagerTask ScaleClose(RectTransform rectTransform, float time)
         {
-            return new TaskManagerTask(ScaleCoroutine(rectTransform, -time), true);
+            return new TaskManagerTask(ScaleTimeCloseCoroutine(rectTransform, time), true);
         }
 
-        static IEnumerator ScaleCoroutine(RectTransform rectTransform, float time)
+        public static TaskManagerTask ScaleClose(RectTransform rectTransform, AnimationCurve curve, float timeMultiplier = 1)
         {
-            Vector3 initialScale = rectTransform.localScale;
+            return new TaskManagerTask(ScaleCurveCloseCoroutine(rectTransform, curve, timeMultiplier), true);
+        }
+
+        static IEnumerator ScaleTimeOpenCoroutine(RectTransform rectTransform, float time)
+        {
             float delta = 0;
 
-            if (time > 0)
+            while(rectTransform.localScale.x < 1)
             {
-                while(rectTransform.localScale.x < 1)
-                {
-                    delta = Time.deltaTime/time;
-                    rectTransform.localScale += new Vector3(delta, delta, 0);
-                    yield return null;
-                }
-            } else if (time < 0)
-            {
-                while(rectTransform.localScale.x > 0)
-                {
-                    delta = Time.deltaTime/time;
-                    rectTransform.localScale += new Vector3(delta, delta, 0);
-                    yield return null;
-                }
+                delta = Time.deltaTime/time;
+                rectTransform.localScale += new Vector3(delta, delta, 0);
+                yield return null;
             }
+
+            rectTransform.localScale = new Vector3(1, 1, 1);
+        }
+
+        static IEnumerator ScaleTimeCloseCoroutine(RectTransform rectTransform, float time)
+        {
+            float delta = 0;
+
+            while(rectTransform.localScale.x > 0)
+            {
+                delta = Time.deltaTime/time;
+                rectTransform.localScale -= new Vector3(delta, delta, 0);
+                yield return null;
+            }
+
+            rectTransform.localScale = new Vector3(0, 0, 1);
+        }
+
+        static IEnumerator ScaleCurveOpenCoroutine(RectTransform rectTransform, AnimationCurve curve, float timeMultiplier = 1)
+        {
+            float time = 0;
+            float value = 0;
+            while(rectTransform.localScale.x < 1)
+            {
+                value = curve.Evaluate(time);
+                rectTransform.localScale = new Vector3(value, value, 0);
+                time += Time.deltaTime/timeMultiplier;
+                yield return null;
+            }
+
+            rectTransform.localScale = new Vector3(1, 1, 1);
+        }
+
+        static IEnumerator ScaleCurveCloseCoroutine(RectTransform rectTransform, AnimationCurve curve, float timeMultiplier = 1)
+        {
+            float time = 0;
+            float value = 0;
+            while(rectTransform.localScale.x > 0)
+            {
+                value = curve.Evaluate(time);
+                rectTransform.localScale = new Vector3(value, value, 0);
+                time += Time.deltaTime/timeMultiplier;
+                yield return null;
+            }
+
+            rectTransform.localScale = new Vector3(0, 0, 1);
         }
     }
 }
