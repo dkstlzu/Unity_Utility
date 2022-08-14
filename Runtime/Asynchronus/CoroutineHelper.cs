a988a025ae2730c1e3fcb0a79b34881254ec4924
Shin SeockJu <margron@naver.com>
2022-08-14 12:11:07 +0900

    CoroutineHelper.AfterTime added

--- Runtime/Asynchronus/CoroutineHelper.cs
+++ Runtime/Asynchronus/CoroutineHelper.cs
@@ -1,18 +1,34 @@
 using System;
 using System.Collections;
 
+using UnityEngine;
+
 namespace dkstlzu.Utility
 {
     public class CoroutineHelper : Singleton<CoroutineHelper>
     {
         public static void OnNextFrame(Action action)
         {
-            CoroutineHelper.GetOrCreateInstance.StartCoroutine(NextFrame(action));
+            CoroutineHelper.GetOrCreateInstance.StartCoroutine(AfterFrame(action, 1));
         }
 
-        static IEnumerator NextFrame(Action action)
+        public static void Delay(Action action, float time)
         {
-            yield return null;
+            CoroutineHelper.GetOrCreateInstance.StartCoroutine(AfterTime(action, time));
+        }
+        
+        static IEnumerator AfterFrame(Action action, int frame)
+        {
+            while(frame-- > 0)
+            {
+                yield return null;
+            }
+            action.Invoke();
+        }
+
+        static IEnumerator AfterTime(Action action, float time)
+        {
+            yield return new WaitForSeconds(time);
             action.Invoke();
         }
     }
