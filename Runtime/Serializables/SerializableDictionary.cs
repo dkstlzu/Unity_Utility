f4542503e38e715e8a07f3d9062d22b35877b17c
Shin SeockJu <margron@naver.com>
2022-08-30 06:05:36 +0900

    SerializableDictionary replaced by AssetStore package

--- Runtime/Serializables/SerializableDictionary.cs
+++ Runtime/Serializables/SerializableDictionary.cs
@@ -1,41 +1,129 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using UnityEngine;
-
 
 namespace dkstlzu.Utility
 {
-    [System.Serializable]
-    public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
-    {
-        [SerializeField]
-        private List<TKey> keys = new List<TKey>();
+	public abstract class SerializableDictionaryBase<TKey, TValue, TValueStorage> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+	{
+		[SerializeField]
+		TKey[] m_keys;
+		[SerializeField]
+		TValueStorage[] m_values;
 
-        [SerializeField]
-        private List<TValue> values = new List<TValue>();
+		public SerializableDictionaryBase()
+		{
+		}
 
-        // save the dictionary to lists
-        public void OnBeforeSerialize()
-        {
-            keys.Clear();
-            values.Clear();
-            foreach (KeyValuePair<TKey, TValue> pair in this)
-            {
-                keys.Add(pair.Key);
-                values.Add(pair.Value);
-            }
-        }
+		public SerializableDictionaryBase(IDictionary<TKey, TValue> dict) : base(dict.Count)
+		{
+			foreach (var kvp in dict)
+			{
+				this[kvp.Key] = kvp.Value;
+			}
+		}
+		
+		protected SerializableDictionaryBase(SerializationInfo info, StreamingContext context) : base(info,context){}
 
-        // load dictionary from lists
-        public void OnAfterDeserialize()
-        {
-            this.Clear();
+		protected abstract void SetValue(TValueStorage[] storage, int i, TValue value);
+		protected abstract TValue GetValue(TValueStorage[] storage, int i);
 
-            if (keys.Count != values.Count)
-                throw new System.Exception(string.Format("there are {0} keys and {1} values after deserialization. Make sure that both key and value types are serializable."));
+		public void CopyFrom(IDictionary<TKey, TValue> dict)
+		{
+			this.Clear();
+			foreach (var kvp in dict)
+			{
+				this[kvp.Key] = kvp.Value;
+			}
+		}
 
-            for (int i = 0; i < keys.Count; i++)
-                this.Add(keys[i], values[i]);
-        }
-    }
+		public void OnAfterDeserialize()
+		{
+			if(m_keys != null && m_values != null && m_keys.Length == m_values.Length)
+			{
+				this.Clear();
+				int n = m_keys.Length;
+				for(int i = 0; i < n; ++i)
+				{
+					this[m_keys[i]] = GetValue(m_values, i);
+				}
+
+				m_keys = null;
+				m_values = null;
+			}
+
+		}
+
+		public void OnBeforeSerialize()
+		{
+			int n = this.Count;
+			m_keys = new TKey[n];
+			m_values = new TValueStorage[n];
+
+			int i = 0;
+			foreach(var kvp in this)
+			{
+				m_keys[i] = kvp.Key;
+				SetValue(m_values, i, kvp.Value);
+				++i;
+			}
+		}
+	}
+
+	public class SerializableDictionary<TKey, TValue> : SerializableDictionaryBase<TKey, TValue, TValue>
+	{
+		public SerializableDictionary()
+		{
+		}
+
+		public SerializableDictionary(IDictionary<TKey, TValue> dict) : base(dict)
+		{
+		}
+
+		protected SerializableDictionary(SerializationInfo info, StreamingContext context) : base(info,context){}
+
+		protected override TValue GetValue(TValue[] storage, int i)
+		{
+			return storage[i];
+		}
+
+		protected override void SetValue(TValue[] storage, int i, TValue value)
+		{
+			storage[i] = value;
+		}
+	}
+
+	public static class SerializableDictionary
+	{
+		public class Storage<T>
+		{
+			public T data;
+		}
+	}
+
+	public class SerializableDictionary<TKey, TValue, TValueStorage> : SerializableDictionaryBase<TKey, TValue, TValueStorage> where TValueStorage : SerializableDictionary.Storage<TValue>, new()
+	{
+		public SerializableDictionary()
+		{
+		}
+
+		public SerializableDictionary(IDictionary<TKey, TValue> dict) : base(dict)
+		{
+		}
+
+		protected SerializableDictionary(SerializationInfo info, StreamingContext context) : base(info,context){}
+
+		protected override TValue GetValue(TValueStorage[] storage, int i)
+		{
+			return storage[i].data;
+		}
+
+		protected override void SetValue(TValueStorage[] storage, int i, TValue value)
+		{
+			storage[i] = new TValueStorage();
+			storage[i].data = value;
+		}
+	}
 }
