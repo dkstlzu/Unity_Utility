ef15f32c6bbd06bc6b60430acbc35f0e23e153c8
SJ is free <dkstlzu@gmail.com>
2024-07-06 13:38:17 +0900

    UpdateManagerMonoBehaviour file divided

--- Runtime/UpdateManager.cs
+++ Runtime/UpdateManager.cs
@@ -4,43 +4,6 @@ using UnityEngine;
 
 namespace dkstlzu.Utility
 {
-    [DefaultExecutionOrder(-10)]
-    [AddComponentMenu("UpdateManager")]
-    public class UpdateManagerMonoBehaviour : Singleton<UpdateManagerMonoBehaviour>
-    {
-        private UpdateManager _updateManager;
-        private FixedUpdateManager _fixedUpdateManager;
-
-#if UNITY_EDITOR
-        public int UpdatableNumber;
-        public int FixedUpdatableNumber;
-#endif
-
-        private void Awake()
-        {
-            _updateManager = new UpdateManager();
-            _fixedUpdateManager = new FixedUpdateManager();
-            
-            Singleton.RegisterSingleton(_updateManager);
-            Singleton.RegisterSingleton(_fixedUpdateManager);
-        }
-
-        private void Update()
-        {
-            _updateManager.Update(default);
-
-#if UNITY_EDITOR
-            UpdatableNumber = _updateManager.Count;
-            FixedUpdatableNumber = _fixedUpdateManager.Count;
-#endif
-        }
-
-        private void FixedUpdate()
-        {
-            _fixedUpdateManager.Update(default);
-        }
-    }
-    
     public interface IUpdatable<T>
     {
         void Update(T t);
---
+++ Runtime/UpdateManagerMonoBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace dkstlzu.Utility
+{
+    [DefaultExecutionOrder(-10)]
+    [AddComponentMenu("UpdateManager")]
+    public class UpdateManagerMonoBehaviour : MonoBehaviour
+    {
+        private UpdateManager _updateManager;
+        private FixedUpdateManager _fixedUpdateManager;
+
+#if UNITY_EDITOR
+        public int UpdatableNumber;
+        public int FixedUpdatableNumber;
+#endif
+
+        private void Awake()
+        {
+            _updateManager = new UpdateManager();
+            _fixedUpdateManager = new FixedUpdateManager();
+            
+            Singleton.RegisterSingleton(_updateManager);
+            Singleton.RegisterSingleton(_fixedUpdateManager);
+        }
+
+        private void Update()
+        {
+            _updateManager.Update(default);
+
+#if UNITY_EDITOR
+            UpdatableNumber = _updateManager.Count;
+            FixedUpdatableNumber = _fixedUpdateManager.Count;
+#endif
+        }
+
+        private void FixedUpdate()
+        {
+            _fixedUpdateManager.Update(default);
+        }
+    }
+}
