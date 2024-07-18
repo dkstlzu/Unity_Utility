be5a743929ad722e41cc2b9a066fe1cb3d2265ae
SJ is free <dkstlzu@gmail.com>
2024-07-18 16:35:13 +0900

    WindowPrinter added

---
+++ Runtime/WindowPrinter.cs
@@ -0,0 +1,44 @@
+﻿namespace dkstlzu.Utility
+{
+    public static class WindowPrinter
+    {
+        private static WindowPrinterMonoBehaviour _instance;
+
+        public static float Duration
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = WindowPrinterMonoBehaviour.GetOrCreateDontDestroyOnLoad();
+                }
+                
+                return _instance.PrintDuration;
+            }
+
+            set
+            {
+                if (_instance == null)
+                {
+                    _instance = WindowPrinterMonoBehaviour.GetOrCreateDontDestroyOnLoad();
+                }
+                
+                _instance.PrintDuration = value;
+            }
+        }
+        
+        public static void Print(object message, string customTag = null, int priority = 0)
+        {
+            if (priority < Printer.CurrentPriority) return;
+
+            string result = Printer.BracketTag(customTag) + message.ToString() + "\n";
+
+            if (_instance == null)
+            {
+                _instance = WindowPrinterMonoBehaviour.GetOrCreateDontDestroyOnLoad();
+            }
+            
+            _instance.Enqueue(result);
+        }
+    }
+}
---
+++ Runtime/WindowPrinterMonoBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace dkstlzu.Utility
+{
+    internal class WindowPrinterMonoBehaviour : Singleton<WindowPrinterMonoBehaviour>
+    {
+        struct WindowPrinterTimer
+        {
+            public string Message;
+            public float InvokedTime;
+        }
+        
+        public Rect Box = new Rect(50, 50, 500, 1000);
+        public float PrintDuration = 3f;
+        private Queue<WindowPrinterTimer> msgs = new Queue<WindowPrinterTimer>();
+
+        public void Enqueue(string str)
+        {
+            msgs.Enqueue(new WindowPrinterTimer()
+            {
+                Message = str,
+                InvokedTime = Time.realtimeSinceStartup
+            });
+        }
+        
+        private void OnGUI()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+                
+            var enumerator = msgs.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                stringBuilder.AppendLine(enumerator.Current.Message);
+            }
+
+            GUI.skin.label.fontSize = 20;
+            GUI.Label(Box, stringBuilder.ToString());
+        }
+
+        private void Update()
+        {
+            if (msgs.TryPeek(out WindowPrinterTimer timer))
+            {
+                if (timer.InvokedTime + PrintDuration < Time.realtimeSinceStartup)
+                {
+                    msgs.Dequeue();
+                }
+            }
+        }
+    }
+}
