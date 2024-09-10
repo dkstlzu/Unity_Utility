258917864312440413754864d9cc53bbcfc0fb5d
sjisfree <isfree@fathermade.net>
2024-09-10 11:23:09 +0900

    ScriptableSingleton added

--- Runtime/Singleton.cs
+++ Runtime/Singleton.cs
@@ -1,6 +1,7 @@
 ﻿using JetBrains.Annotations;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -127,6 +128,54 @@ namespace dkstlzu.Utility
                 var go = new GameObject($"{typeof(T)} Singleton");
                 DontDestroyOnLoad(go);
                 _instance = go.AddComponent<T>();
+            }
+
+            return _instance;
+        }
+    }
+
+    public class ScriptableSingleton<T> : ScriptableObject where T : ScriptableObject
+    {
+        private const string RESOURCES_FOLDER_NAME = "Resources";
+        public const string FOLDER_NAME = "Scriptables";
+        public static string ResourcesPath => Path.Combine(FOLDER_NAME, typeof(T).Name);
+        
+        private static T _instance;
+
+        public static T GetOrNull
+        {
+            get
+            {
+                if (!_instance)
+                {
+                    _instance = Resources.Load<T>(ResourcesPath);
+                }
+                
+                return _instance;
+            }
+        }
+
+        public static T GetOrCreate()
+        {
+            _instance = GetOrNull;
+
+            if (!_instance)
+            {
+                _instance = CreateInstance<T>();
+#if UNITY_EDITOR
+                string folderPath = Path.Combine(Application.dataPath, RESOURCES_FOLDER_NAME, FOLDER_NAME);
+                
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                    UnityEditor.AssetDatabase.Refresh();
+                }
+
+                string path = Path.Combine("Assets", RESOURCES_FOLDER_NAME, $"{ResourcesPath}.asset");
+                UnityEditor.AssetDatabase.CreateAsset(_instance, path);
+                UnityEditor.AssetDatabase.SaveAssets();
+                UnityEditor.AssetDatabase.Refresh();
+#endif
             }
 
             return _instance;
