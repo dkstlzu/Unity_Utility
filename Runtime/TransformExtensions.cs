003dadf3ad93e58ae8d9bdb5a9d9d76daa815339
dkstlzu <dkstlzu@gmail.com>
2025-09-29 17:54:51 +0900

    Trasform traverse Parent implemented

--- Runtime/TransformExtensions.cs
+++ Runtime/TransformExtensions.cs
@@ -40,6 +40,40 @@ namespace dkstlzu.Utility
             }
         }
     
+        public static void TraverseParents(this Transform transform, Action<Transform> action, Func<Transform, bool> breakAction = null)
+        {
+            if (breakAction != null)
+            {
+                if (breakAction(transform))
+                {
+                    return;
+                }
+            }
+            action?.Invoke(transform);
+
+            if (transform.parent != null)
+            {
+                transform.parent.TraverseParents(action, breakAction);
+            }
+        }
+        
+        public static void TraverseParentsWithDepth(this Transform transform, Action<Transform, int> action)
+        {
+            int depth = 0;
+            TraverseParentsWithDepth_Recursive(transform, action, depth);
+        }
+
+        private static void TraverseParentsWithDepth_Recursive(this Transform transform, Action<Transform, int> action, int depth)
+        {
+            action?.Invoke(transform, depth);
+
+            depth++;
+            foreach (Transform child in transform)
+            {
+                child.TraverseDescendentsWithDepth_Recursive(action, depth);
+            }
+        }
+        
         public static string PrintChildren(this Transform transform)
         {
             StringBuilder stringBuilder = new StringBuilder();
