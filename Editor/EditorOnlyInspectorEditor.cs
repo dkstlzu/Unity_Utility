df02ea292dc1e471e7722c103a6b50865f506072
dkstlzu <dkstlzu@gmail.com>
2025-10-25 17:36:23 +0900

    EditorOnlyInspector implemented

---
+++ Editor/EditorOnlyInspectorEditor.cs
@@ -0,0 +1,19 @@
+﻿using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+[CustomEditor(typeof(EditorOnlyInspector))]
+public class EditorOnlyInspectorEditor : Editor
+{
+    public override VisualElement CreateInspectorGUI()
+    {
+        var root = new VisualElement();
+
+        SerializedProperty property = serializedObject.FindProperty("obj");
+        PropertyField propertyField = new PropertyField(property, property.managedReferenceValue.GetType().Name);
+        root.Add(propertyField);
+        
+        return root;
+    }
+}
---
+++ Runtime/EditorOnlyInspector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+using Object = UnityEngine.Object;
+
+public class EditorOnlyInspector : MonoBehaviour
+{
+    [SerializeReference]
+    public object obj;
+
+    private MethodInfo gizmoMethod;
+    private MethodInfo gizmoSelectedMethod;
+    
+    private void Awake()
+    {
+#if !UNITY_EDITOR
+        Destroy(this);
+#endif
+    }
+
+    private void SetValue(object obj)
+    {
+        this.obj = obj;
+        
+        gizmoMethod = obj.GetType()?.GetMethod("OnDrawGizmos", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        gizmoSelectedMethod = obj.GetType()?.GetMethod("OnDrawGizmosSelected", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);   
+    }
+
+#if UNITY_EDITOR
+    void OnDrawGizmos()
+    {
+        gizmoMethod?.Invoke(obj, null);
+    }
+    
+    void OnDrawGizmosSelected()
+    {
+        gizmoSelectedMethod?.Invoke(obj, null);
+    }
+#endif
+
+
+    [Conditional("UNITY_EDITOR")]
+    public static void AddOn(GameObject gameObject, object obj)
+    {
+        gameObject.AddComponent<EditorOnlyInspector>().SetValue(obj);
+    }
+}
