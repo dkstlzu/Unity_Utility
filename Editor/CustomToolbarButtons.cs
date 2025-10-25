f724289b3fb4eba248e75eb466ef0e13feda9ed1
dkstlzu <dkstlzu@gmail.com>
2025-10-26 00:51:58 +0900

    DevGameStart Upgrdae

--- Editor/CustomToolbarButtons.cs
+++ Editor/CustomToolbarButtons.cs
@@ -4,32 +4,51 @@ using UnityEditor;
 using UnityEditor.Overlays;
 using UnityEditor.Toolbars;
 
-[Overlay(typeof(SceneView), "게임 시작", true)]
-public class DevGameStartOverlay : ToolbarOverlay
+namespace dkstlzu.Utility
 {
-    public DevGameStartOverlay() : base((DevGameStartData.Default.SceneList == null || DevGameStartData.Default.SceneList.Count == 0)
-        ? new []{"empty"}
-        : DevGameStartData.Default.SceneList.Select(s => $"{s.name}").ToArray())
+    [Overlay(typeof(SceneView), "게임 시작", true)]
+    public class DevGameStartOverlay : ToolbarOverlay
     {
-    }
-}
-
-public class PlayOnSceneOverlayButton : EditorToolbarButton
-{
-    public PlayOnSceneOverlayButton(string sceneName, string buttonText, Action actionAfterPlay = null) : base(buttonText, () =>
-    {
-        SceneAsset sceneAsset = DevGameStartData.Default.SceneList.Find(scene => scene.name.Contains(sceneName));
-
-        if (sceneAsset == null)
+        public DevGameStartOverlay() : base((DevGameStartData.Default.SceneList == null || DevGameStartData.Default.SceneList.Count == 0)
+            ? new []{"empty"}
+            : DevGameStartData.Default.SceneList.Select(s => $"{s.name}").ToArray())
         {
-            Printer.Print($"{buttonText}-{sceneName} not found from DevGameStartData.Default");
-            return;
         }
-        
-        DevGameStartData.Play(sceneAsset);
-        
-        actionAfterPlay?.Invoke();
-    })
+    }
+
+    public class PlayOnSceneOverlayButton : EditorToolbarButton
     {
+        protected static Action AfterPlay;
+        
+        public PlayOnSceneOverlayButton(string sceneName, string buttonText, Action actionAfterPlay = null) : base(buttonText, () =>
+        {
+            SceneAsset sceneAsset = DevGameStartData.Default.SceneList.Find(scene => scene.name.Contains(sceneName));
+
+            if (sceneAsset == null)
+            {
+                Printer.Print($"{buttonText}-{sceneName} not found from DevGameStartData.Default");
+                return;
+            }
+            
+            EditorApplication.playModeStateChanged += OnPlay;
+            
+            DevGameStartData.Play(sceneAsset);
+        
+            actionAfterPlay?.Invoke();
+        })
+        {
+        }
+
+        private static void OnPlay(PlayModeStateChange change)
+        {
+            if (change != PlayModeStateChange.EnteredPlayMode)
+            {
+                return;
+            }
+
+            AfterPlay?.Invoke();
+            
+            EditorApplication.playModeStateChanged -= OnPlay;
+        }
     }
 }
